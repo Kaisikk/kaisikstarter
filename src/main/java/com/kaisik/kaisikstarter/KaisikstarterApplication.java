b03a03f7b9a420d2cb3bfd88095947118ceff601
Kaisikk <Kosikov.Ilya@mail.ru>
2024-08-29 22:21:05 +0400

    Создание проекта

---
+++ src/main/java/com/kaisik/kaisikstarter/KaisikstarterApplication.java
@@ -0,0 +1,13 @@
+package com.kaisik.kaisikstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaisikstarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaisikstarterApplication.class, args);
+    }
+
+}
